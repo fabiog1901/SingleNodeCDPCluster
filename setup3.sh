7f5bfce4f1a09caa7a35d4e8de7b80db70ffe3b5
fabiog <ghirardello.fabio@gmail.com>
2019-09-30 16:13:23 -0400

    Create setup3.sh

---
+++ setup3.sh
@@ -0,0 +1,174 @@
+#! /bin/bash
+echo "-- Configure and optimize the OS"
+echo never > /sys/kernel/mm/transparent_hugepage/enabled
+echo never > /sys/kernel/mm/transparent_hugepage/defrag
+echo "echo never > /sys/kernel/mm/transparent_hugepage/enabled" >> /etc/rc.d/rc.local
+echo "echo never > /sys/kernel/mm/transparent_hugepage/defrag" >> /etc/rc.d/rc.local
+# add tuned optimization https://www.cloudera.com/documentation/enterprise/6/6.2/topics/cdh_admin_performance.html
+echo  "vm.swappiness = 1" >> /etc/sysctl.conf
+sysctl vm.swappiness=1
+timedatectl set-timezone UTC
+
+echo "-- Install Java OpenJDK8 and other tools"
+yum install -y java-1.8.0-openjdk-devel vim wget curl git bind-utils
+
+# Check input parameters
+case "$1" in
+        aws)
+            echo "server 169.254.169.123 prefer iburst minpoll 4 maxpoll 4" >> /etc/chrony.conf
+            systemctl restart chronyd
+            ;;
+        azure)
+            umount /mnt/resource
+            mount /dev/sdb1 /opt
+            ;;
+        gcp)
+            ;;
+        *)
+            echo $"Usage: $0 {aws|azure|gcp} template-file username password [docker-device]"
+            echo $"example: ./setup.sh azure templates/essential.json fabio my_pass"
+            echo $"example: ./setup.sh aws template/cml.json fabio my_pass /dev/xvdb"
+            exit 1
+esac
+
+TEMPLATE=$2
+USERNAME=$3
+PASSWORD=$4
+DOCKERDEVICE=$5
+
+echo "-- Configure networking"
+PUBLIC_IP=`curl https://api.ipify.org/`
+hostnamectl set-hostname `hostname -f`
+echo "`hostname -I` `hostname`" >> /etc/hosts
+sed -i "s/HOSTNAME=.*/HOSTNAME=`hostname`/" /etc/sysconfig/network
+systemctl disable firewalld
+systemctl stop firewalld
+setenforce 0
+sed -i 's/SELINUX=.*/SELINUX=disabled/' /etc/selinux/config
+
+
+echo "-- Install CM and MariaDB"
+
+## CM 7
+cat - >/etc/yum.repos.d/cloudera-manager.repo <<EOF
+[cm]
+name=cm
+enabled=1
+type=rpm-md
+baseurl=https://$USERNAME:$PASSWORD@archive.cloudera.com/p/cm7/7.x.0/redhat7/yum/
+gpgcheck=0
+EOF
+
+## MariaDB 10.1
+cat - >/etc/yum.repos.d/MariaDB.repo <<EOF
+[mariadb]
+name = MariaDB
+baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
+gpgcheck=1
+EOF
+
+
+yum clean all
+rm -rf /var/cache/yum/
+yum repolist
+
+## CM
+yum install -y cloudera-manager-agent cloudera-manager-daemons cloudera-manager-server
+
+## MariaDB
+yum install -y MariaDB-server MariaDB-client
+cat conf/mariadb.config > /etc/my.cnf
+
+echo "--Enable and start MariaDB"
+systemctl enable mariadb
+systemctl start mariadb
+
+echo "-- Install JDBC connector"
+wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz -P ~
+tar zxf ~/mysql-connector-java-5.1.46.tar.gz -C ~
+mkdir -p /usr/share/java/
+cp ~/mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar /usr/share/java/mysql-connector-java.jar
+rm -rf ~/mysql-connector-java-5.1.46*
+
+echo "-- Create DBs required by CM"
+mysql -u root < scripts/create_db.sql
+
+echo "-- Secure MariaDB"
+mysql -u root < scripts/secure_mariadb.sql
+
+echo "-- Prepare CM database 'scm'"
+/opt/cloudera/cm/schema/scm_prepare_database.sh mysql scm scm cloudera
+
+
+
+## PostgreSQL
+yum install -y postgresql-server python-pip
+
+pip install psycopg2==2.7.5 --ignore-installed
+echo 'LC_ALL="en_US.UTF-8"' >> /etc/locale.conf
+sudo su -l postgres -c "postgresql-setup initdb"
+
+cat conf/pg_hba.conf > /var/lib/pgsql/data/pg_hba.conf
+cat conf/postgresql.conf > /var/lib/pgsql/data/postgresql.conf
+
+
+echo "-- Create DBs required by CM"
+sudo -u postgres psql <<EOF 
+CREATE DATABASE ranger;
+CREATE USER rangeradmin WITH PASSWORD 'supersecret1';
+GRANT ALL PRIVILEGES ON DATABASE ranger TO rangeradmin;
+EOF
+
+
+
+echo "--Enable and start pgsql"
+systemctl enable postgresql
+systemctl restart postgresql
+
+
+
+
+echo "-- Install CSDs"
+# install local CSDs
+mv ~/*.jar /opt/cloudera/csd/
+chown cloudera-scm:cloudera-scm /opt/cloudera/csd/*
+chmod 644 /opt/cloudera/csd/*
+
+echo "-- Install local parcels"
+mv ~/*.parcel ~/*.parcel.sha /opt/cloudera/parcel-repo/
+chown cloudera-scm:cloudera-scm /opt/cloudera/parcel-repo/*
+
+
+echo "-- Enable passwordless root login via rsa key"
+ssh-keygen -f ~/myRSAkey -t rsa -N ""
+mkdir ~/.ssh
+cat ~/myRSAkey.pub >> ~/.ssh/authorized_keys
+chmod 400 ~/.ssh/authorized_keys
+ssh-keyscan -H `hostname` >> ~/.ssh/known_hosts
+sed -i 's/.*PermitRootLogin.*/PermitRootLogin without-password/' /etc/ssh/sshd_config
+systemctl restart sshd
+
+echo "-- Start CM, it takes about 2 minutes to be ready"
+systemctl start cloudera-scm-server
+
+while [ `curl -s -X GET -u "admin:admin"  http://localhost:7180/api/version` -z ] ;
+    do
+    echo "waiting 10s for CM to come up..";
+    sleep 10;
+done
+
+echo "-- Now CM is started and the next step is to automate using the CM API"
+
+yum install -y epel-release
+yum install -y python-pip
+pip install --upgrade pip cm_client
+
+sed -i "s/YourHostname/`hostname -f`/g" $TEMPLATE
+sed -i "s/YourCDSWDomain/cdsw.$PUBLIC_IP.nip.io/g" $TEMPLATE
+sed -i "s/YourPrivateIP/`hostname -I | tr -d '[:space:]'`/g" $TEMPLATE
+sed -i "s#YourDockerDevice#$DOCKERDEVICE#g" $TEMPLATE
+
+sed -i "s/YourHostname/`hostname -f`/g" scripts/create_cluster.py
+
+python scripts/create_cluster.py $TEMPLATE $USERNAME $PASSWORD
