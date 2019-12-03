63096c616ec92a7049328a03ac53ebe520d7c4a3
fabiog <ghirardello.fabio@gmail.com>
2019-12-03 12:18:13 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -150,6 +150,28 @@ echo "-- Install local parcels"
 mv ~/*.parcel ~/*.parcel.sha /opt/cloudera/parcel-repo/
 chown cloudera-scm:cloudera-scm /opt/cloudera/parcel-repo/*
 
+echo "-- Install CEM Tarballs"
+mkdir -p /opt/cloudera/cem
+wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.0.0.0/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/efm/efm-1.0.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-toolkit-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
+rm -f /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz
+ln -s /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54 /opt/cloudera/cem/efm
+ln -s /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54 /opt/cloudera/cem/minifi
+ln -s /opt/cloudera/cem/efm/bin/efm.sh /etc/init.d/efm
+chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54
+chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54
+chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.0.0.0-54
+rm -f /opt/cloudera/cem/efm/conf/efm.properties
+cp conf/efm.properties /opt/cloudera/cem/efm/conf
+rm -f /opt/cloudera/cem/minifi/conf/bootstrap.conf
+cp conf/bootstrap.conf /opt/cloudera/cem/minifi/conf
+sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/efm/conf/efm.properties
+sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/minifi/conf/bootstrap.conf
+/opt/cloudera/cem/minifi/bin/minifi.sh install
+
 
 echo "-- Enable passwordless root login via rsa key"
 ssh-keygen -f ~/myRSAkey -t rsa -N ""
@@ -181,3 +203,7 @@ sed -i "s#YourDockerDevice#$DOCKERDEVICE#g" $TEMPLATE
 sed -i "s/YourHostname/`hostname -f`/g" scripts/create_cluster.py
 
 python scripts/create_cluster.py $TEMPLATE
+
+# configure and start EFM and Minifi
+service efm start
+#service minifi start
