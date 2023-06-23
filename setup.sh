0bba3225156ef096bbcbc0170e59df29dc5d5fe1
David Fowler <dfowler@cloudera.com>
2023-06-23 15:32:52 -0600

    Update setup.sh
    updated to a version of mariadb, and Postgres that work currently.  Trying an updated j connector that worked for my simple tests.  Removing CDSW, NiFI, MiniFi/CEM since those installs have changed.

--- setup.sh
+++ setup.sh
@@ -21,11 +21,11 @@ systemctl daemon-reload
 systemctl start rngd
 systemctl enable rngd
 
-echo "-- Installing requirements for Stream Messaging Manager"
-yum install -y gcc-c++ make 
-curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash - 
-yum install nodejs -y
-npm install forever -g 
+#echo "-- Installing requirements for Stream Messaging Manager"
+#yum install -y gcc-c++ make 
+#curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash - 
+#yum install nodejs -y
+#npm install forever -g 
 
 # Check input parameters
 case "$1" in
@@ -70,11 +70,11 @@ echo "-- Install CM and MariaDB"
 ## CM 7
 wget https://archive.cloudera.com/cm7/7.4.4/redhat7/yum/cloudera-manager-trial.repo -P /etc/yum.repos.d/
 
-## MariaDB 10.1
+## MariaDB 10.3 -  - 10.4 and above changes from table to view for mysql.user and would needs more changes
 cat - >/etc/yum.repos.d/MariaDB.repo <<EOF
 [mariadb]
 name = MariaDB
-baseurl = http://yum.mariadb.org/10.1/centos7-amd64
+baseurl = http://yum.mariadb.org/10.3/centos7-amd64
 gpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB
 gpgcheck=1
 EOF
@@ -96,11 +96,12 @@ systemctl enable mariadb
 systemctl start mariadb
 
 echo "-- Install JDBC connector"
-wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz -P ~
-tar zxf ~/mysql-connector-java-5.1.46.tar.gz -C ~
+#wget https://dev.mysql.com/get/Downloads/Connector-J/mysql-connector-java-5.1.46.tar.gz -P ~ - trying a slightly newer version below
+wget https://downloads.mysql.com/archives/get/p/3/file/mysql-connector-java-5.1.49.tar.gz -P ~
+tar zxf ~/mysql-connector-java-5.1.49.tar.gz -C ~
 mkdir -p /usr/share/java/
-cp ~/mysql-connector-java-5.1.46/mysql-connector-java-5.1.46-bin.jar /usr/share/java/mysql-connector-java.jar
-rm -rf ~/mysql-connector-java-5.1.46*
+cp ~/mysql-connector-java-5.1.49/mysql-connector-java-5.1.49-bin.jar /usr/share/java/mysql-connector-java.jar
+rm -rf ~/mysql-connector-java-5.1.49*
 
 echo "-- Create DBs required by CM"
 mysql -u root < scripts/create_db.sql
@@ -111,33 +112,28 @@ mysql -u root < scripts/secure_mariadb.sql
 echo "-- Prepare CM database 'scm'"
 /opt/cloudera/cm/schema/scm_prepare_database.sh mysql scm scm cloudera
 
-## PostgreSQL
-#yum install -y postgresql-server python-pip
-#pip install psycopg2==2.7.5 --ignore-installed
-#echo 'LC_ALL="en_US.UTF-8"' >> /etc/locale.conf
-#sudo su -l postgres -c "postgresql-setup initdb"
-#cat conf/pg_hba.conf > /var/lib/pgsql/data/pg_hba.conf
-#cat conf/postgresql.conf > /var/lib/pgsql/data/postgresql.conf
-#echo "--Enable and start pgsql"
-#systemctl enable postgresql
-#systemctl restart postgresql
-
 
 ## PostgreSQL see: https://www.postgresql.org/download/linux/redhat/
 yum install -y https://download.postgresql.org/pub/repos/yum/reporpms/EL-7-x86_64/pgdg-redhat-repo-latest.noarch.rpm
-yum install -y postgresql96
-yum install -y postgresql96-server
+
+#yum install -y postgresql
+sudo yum install -y postgresql11-server
+
 pip install psycopg2==2.7.5 --ignore-installed
+#pip install psycopg2==2.9.5 --ignore-installed - got some erros to fix yet on this version install needed for later CDP Base versions
 
 echo 'LC_ALL="en_US.UTF-8"' >> /etc/locale.conf
-/usr/pgsql-9.6/bin/postgresql96-setup initdb
 
-cat conf/pg_hba.conf > /var/lib/pgsql/9.6/data/pg_hba.conf
-cat conf/postgresql.conf > /var/lib/pgsql/9.6/data/postgresql.conf
+/usr/pgsql-11/bin/postgresql-11-setup initdb
+
+cat conf/pg_hba.conf > /var/lib/pgsql/11/data/pg_hba.conf
+cat conf/postgresql.conf > /var/lib/pgsql/11/data/postgresql.conf
 
 echo "--Enable and start pgsql"
-systemctl enable postgresql-9.6
-systemctl start postgresql-9.6
+#systemctl enable postgresql-9.6
+#systemctl start postgresql-9.6
+sudo systemctl enable postgresql-11
+sudo systemctl start postgresql-11
 
 echo "-- Create DBs required by CM"
 sudo -u postgres psql <<EOF 
@@ -151,19 +147,7 @@ EOF
 
 
 echo "-- Install CSDs"
-wget https://archive.cloudera.com/CFM/csd/1.0.1.0/NIFI-1.9.0.1.0.1.0-12.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/CFM/csd/1.0.1.0/NIFICA-1.9.0.1.0.1.0-12.jar -P /opt/cloudera/csd/
-wget https://archive.cloudera.com/CFM/csd/1.0.1.0/NIFIREGISTRY-0.3.0.1.0.1.0-12.jar -P /opt/cloudera/csd/
-# CDSW CSD: must update descriptors so it can install on CR7
-wget https://archive.cloudera.com/cdsw1/1.6.1/csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.1.jar -P cdswjar
-cd cdswjar/
-jar xvf CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.1.jar 
-rm -rf CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.1.jar 
-sed -i 's/"max" : "6"/"max" : "7"/' descriptor/service.sdl 
-sed -i 's/<cdh.max.version>6/<cdh.max.version>7/' META-INF/maven/com.cloudera.csd/CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6/pom.xml
-jar cvf CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.1.jar *
-mv CLOUDERA_DATA_SCIENCE_WORKBENCH-CDH6-1.6.1.jar ~
-cd ..
+
 
 # install local CSDs
 mv ~/*.jar /opt/cloudera/csd/
@@ -175,28 +159,6 @@ echo "-- Install local parcels"
 mv ~/*.parcel ~/*.parcel.sha /opt/cloudera/parcel-repo/
 mv /home/centos/*.parcel /home/centos/*.parcel.sha /opt/cloudera/parcel-repo/
 chown cloudera-scm:cloudera-scm /opt/cloudera/parcel-repo/*
-
-echo "-- Install CEM Tarballs"
-mkdir -p /opt/cloudera/cem
-wget https://archive.cloudera.com/CEM/centos7/1.x/updates/1.0.0.0/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -P /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/efm/efm-1.0.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-tar xzf /opt/cloudera/cem/CEM/centos7/1.0.0.0-54/tars/minifi/minifi-toolkit-0.6.0.1.0.0.0-54-bin.tar.gz -C /opt/cloudera/cem
-rm -f /opt/cloudera/cem/CEM-1.0.0.0-centos7-tars-tarball.tar.gz
-ln -s /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54 /opt/cloudera/cem/efm
-ln -s /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54 /opt/cloudera/cem/minifi
-ln -s /opt/cloudera/cem/efm/bin/efm.sh /etc/init.d/efm
-chown -R root:root /opt/cloudera/cem/efm-1.0.0.1.0.0.0-54
-chown -R root:root /opt/cloudera/cem/minifi-0.6.0.1.0.0.0-54
-chown -R root:root /opt/cloudera/cem/minifi-toolkit-0.6.0.1.0.0.0-54
-rm -f /opt/cloudera/cem/efm/conf/efm.properties
-cp conf/efm.properties /opt/cloudera/cem/efm/conf
-rm -f /opt/cloudera/cem/minifi/conf/bootstrap.conf
-cp conf/bootstrap.conf /opt/cloudera/cem/minifi/conf
-sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/efm/conf/efm.properties
-sed -i "s/YourHostname/`hostname -f`/g" /opt/cloudera/cem/minifi/conf/bootstrap.conf
-/opt/cloudera/cem/minifi/bin/minifi.sh install
 
 
 echo "-- Enable passwordless root login via rsa key"
@@ -226,14 +188,12 @@ pip install --upgrade cm_client
 echo "-- Kicking off install from ${start_dir}"
 cd ${start_dir}
 sed -i "s/YourHostname/`hostname -f`/g" $TEMPLATE
-sed -i "s/YourCDSWDomain/cdsw.$PUBLIC_IP.nip.io/g" $TEMPLATE
+#sed -i "s/YourCDSWDomain/cdsw.$PUBLIC_IP.nip.io/g" $TEMPLATE
 sed -i "s/YourPrivateIP/`hostname -I | tr -d '[:space:]'`/g" $TEMPLATE
-sed -i "s#YourDockerDevice#$DOCKERDEVICE#g" $TEMPLATE
+#sed -i "s#YourDockerDevice#$DOCKERDEVICE#g" $TEMPLATE
 
 sed -i "s/YourHostname/`hostname -f`/g" scripts/create_cluster.py
 
 python scripts/create_cluster.py $TEMPLATE
 
-# configure and start EFM and Minifi
-service efm start
-#service minifi start
+
